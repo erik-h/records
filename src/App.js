3024eac0796960c292fcd33e77b8341b7f2200cb
Erik H <public+git@erikh.me>
2018-08-22 00:15:18 -0600

    Implement skeleton for adding a record
    A dialog asking for the record's name and phone number pops up when clicking
    the add button. Right now we just log the new user to the console instead
    of storing them in some sort of state.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,129 @@
 import React from 'react';
 import BookIcon from '@material-ui/icons/Book';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
 
 import './App.css';
 import NavBar from './NavBar';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.handler = this.handler.bind(this);
+		this.state = {
+			showAdd: false,
+			showDelete: false,
+			showSearch: false,
+
+			name: "",
+			phoneNumber: "",
+		};
+	}
+
+	handler(newState) {
+		this.setState(newState);
+	}
+
+	validName(name) {
+		return name;
+	}
+
+	validPhoneNumber(phoneNumber) {
+		return phoneNumber && /^\d+$/.test(phoneNumber);
+	}
+
+	handleChange(e) {
+		const key = e.target.name;
+		const value = e.target.value;
+		this.setState({
+			[key]: value
+		});
+	}
+
+	handleCloseAddDialog(shouldAdd) {
+		console.log(`shouldAdd is: ${shouldAdd}`);
+		const newName = this.state.name;
+		const newPhoneNumber = this.state.phoneNumber;
+		this.setState({
+			showAdd: false,
+			name: "",
+			phoneNumber: "",
+		});
+
+		if (!shouldAdd) {
+			return;
+		}
+		else if (this.validName(newName) && this.validPhoneNumber(newPhoneNumber)) {
+			console.log('Doing all the important adding stuff...');
+			console.log(`newRecord name is: ${JSON.stringify(this.state.name)}`);
+			console.log(`newRecord phone number is: ${JSON.stringify(this.state.phoneNumber)}`);
+		}
+		else {
+			console.error('New record is invalid! It will not be added');
+		}
+
+	}
+
   render() {
     return (
       <div className="App">
-				<NavBar>
+				<NavBar handler={this.handler}>
 				</NavBar>
 				<div id="project-info">
 					<BookIcon className="book-logo" color="primary" />
 					<p><b>TODO</b>, this will contain some info about the project.</p>
+					<p>Show add is: {this.state.showAdd ? "true" : "false"}</p>
 				</div>
+				<Dialog
+					open={this.state.showAdd}
+					onClose={this.handleCloseAddDialog.bind(this, false)}
+				>
+					<DialogTitle>Test Dialog</DialogTitle>
+					<DialogContent>
+						<DialogContentText>
+							Test dialog content is right here!
+						</DialogContentText>
+						<TextField
+							onChange={this.handleChange.bind(this)}
+							autoFocus
+							name="name"
+							label="Name"
+							fullWidth
+						/>
+						<TextField
+							onChange={this.handleChange.bind(this)}
+							name="phoneNumber"
+							label="Phone Number"
+							fullWidth
+						/>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={this.handleCloseAddDialog.bind(this, false)}>
+							Cancel
+						</Button>
+						<Button onClick={this.handleCloseAddDialog.bind(this, true)}>
+							Add
+						</Button>
+					</DialogActions>
+				</Dialog>
       </div>
     );
   }
 }
 
+/*
+				<AddRecordDialog
+					open={this.state.showAdd}
+					onClose={this.handleCloseAddDialog.bind(this, false)}
+					onChange={this.handleChange.bind(this)}
+					handleCancel={this.handleCloseAddDialog.bind(this, false)}
+					handleAdd={this.handleCloseAddDialog.bind(this, true)}
+				/>
+*/
 export default App;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
-import DeleteIcon from '@material-ui/icons/Delete';
-import SearchIcon from '@material-ui/icons/Search';
+import AddRecordButton from './AddRecordButton.js';
+import DeleteRecordButton from './DeleteRecordButton.js';
+import SearchRecordButton from './SearchRecordButton.js';
 import Toolbar from '@material-ui/core/Toolbar';
-
-import './NavBar.css';
 
 class NavBar extends React.Component {
 	render() {
 		return (
 			<AppBar position="static">
 				<Toolbar>
-					<Button className="nav-bar-button" variant="contained" color="primary">
-						<AddIcon />
-					</Button>
-					<Button className="nav-bar-button" variant="contained" color="primary">
-						<DeleteIcon />
-					</Button>
-					<Button className="nav-bar-button" variant="contained" color="primary">
-						<SearchIcon />
-					</Button>
+					<AddRecordButton handler={this.props.handler}></AddRecordButton>
+					<DeleteRecordButton handler={this.props.handler}></DeleteRecordButton>
+					<SearchRecordButton handler={this.props.handler}></SearchRecordButton>
 				</Toolbar>
 			</AppBar>
 		);
